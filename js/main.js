2bdf28a0a0669c2aa8b7d4275df2c1460e568b0b
= <me@mail.ru>
2020-02-21 21:06:03 +0300

    disabled move over the canvas

--- js/main.js
+++ js/main.js
@@ -13,6 +13,49 @@ $(document).ready(function(){
 	   		originY: 'top'
 	});
 
+	// запрещаем перемещать объект за пределы canvas
+	canvas.on('object:moving', function (e) {
+		const obj = e.target;
+		const rectTop = obj.getBoundingRect().top;
+		const rectLeft = obj.getBoundingRect().left;
+		const rectWidth = obj.getBoundingRect().width;
+		const rectHeight = obj.getBoundingRect().height;
+
+		//if object is too big ignore
+		if (
+			obj.currentHeight > obj.canvas.height
+			|| obj.currentWidth > obj.canvas.width
+		){
+		    return;
+		}
+
+		obj.setCoords();
+
+		//top-left corner
+		if (
+			rectTop < 0
+			|| rectLeft < 0
+		){
+		    obj.top = Math.max(obj.top, obj.top - rectTop);
+		    obj.left = Math.max(obj.left, obj.left - rectLeft);
+		}
+
+		//bot-right corner
+		if (
+			rectTop + rectHeight > obj.canvas.height
+			|| rectLeft + rectWidth > obj.canvas.width
+		){
+		    obj.top = Math.min(
+		    	obj.top,
+		    	obj.canvas.height - rectHeight + obj.top - rectTop
+		    );
+		    obj.left = Math.min(
+		    	obj.left,
+		    	obj.canvas.width - rectWidth + obj.left - rectLeft
+		    );
+		}
+	});
+
 
 	let theMoon;
 
