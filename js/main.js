287590cd5944561567f8ae797bb7921cf4793733
= <me@mail.ru>
2020-02-21 23:11:33 +0300

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,106 +1,101 @@
-$(document).ready(function(){
+const disableMoveOver = (canvas) => {
+    canvas.on('object:moving', function (e) {
+        const obj = e.target;
+        const rectTop = obj.getBoundingRect().top;
+        const rectLeft = obj.getBoundingRect().left;
+        const rectWidth = obj.getBoundingRect().width;
+        const rectHeight = obj.getBoundingRect().height;
 
-	const canvas = new fabric.Canvas('FallField', {
-		backgroundColor: '#eee',
-		selection: false,
-		skipOffscreen: true
-	});
+        obj.setCoords();
 
-	canvas.setBackgroundImage('img/background.jpg', canvas.renderAll.bind(canvas), {
-	   		width: canvas.width,
-	   		height: canvas.height,
-	   		originX: 'left',
-	   		originY: 'top'
-	});
+        //top-left corner
+        if ( rectTop < 0 || rectLeft < 0 ){
+            obj.top = Math.max(obj.top, obj.top - rectTop);
+            obj.left = Math.max(obj.left, obj.left - rectLeft);
+        }
 
-	// запрещаем перемещать объект за пределы canvas
-	canvas.on('object:moving', function (e) {
-		const obj = e.target;
-		const rectTop = obj.getBoundingRect().top;
-		const rectLeft = obj.getBoundingRect().left;
-		const rectWidth = obj.getBoundingRect().width;
-		const rectHeight = obj.getBoundingRect().height;
+        //bot-right corner
+        if (
+            rectTop + rectHeight > obj.canvas.height
+            || rectLeft + rectWidth > obj.canvas.width
+        ){
+            obj.top = Math.min(
+                obj.top,
+                obj.canvas.height - rectHeight + obj.top - rectTop
+            );
+            obj.left = Math.min(
+                obj.left,
+                obj.canvas.width - rectWidth + obj.left - rectLeft
+            );
+        }
+    });
+};
 
-		//if object is too big ignore
-		if (
-			obj.currentHeight > obj.canvas.height
-			|| obj.currentWidth > obj.canvas.width
-		){
-		    return;
-		}
+const buildCanvas = () => {
+    const canvas = new fabric.Canvas('FallField', {
+        backgroundColor: '#eee',
+        selection: false,
+        skipOffscreen: true
+    });
 
-		obj.setCoords();
+    canvas.setBackgroundImage('img/background.jpg', canvas.renderAll.bind(canvas), {
+        width: canvas.width,
+        height: canvas.height,
+        originX: 'left',
+        originY: 'top'
+    });
 
-		//top-left corner
-		if (
-			rectTop < 0
-			|| rectLeft < 0
-		){
-		    obj.top = Math.max(obj.top, obj.top - rectTop);
-		    obj.left = Math.max(obj.left, obj.left - rectLeft);
-		}
-
-		//bot-right corner
-		if (
-			rectTop + rectHeight > obj.canvas.height
-			|| rectLeft + rectWidth > obj.canvas.width
-		){
-		    obj.top = Math.min(
-		    	obj.top,
-		    	obj.canvas.height - rectHeight + obj.top - rectTop
-		    );
-		    obj.left = Math.min(
-		    	obj.left,
-		    	obj.canvas.width - rectWidth + obj.left - rectLeft
-		    );
-		}
-	});
-
-	// скрываем подсказку
+    // скрываем подсказку
     canvas.on('object:moving', function () {
         $( "#tip p" ).text("");
     });
 
+    // запрещаем перемещение луны за пределы canvas
+    disableMoveOver(canvas);
+
+    return canvas;
+};
 
 
-	let theMoon, groundLine;
+$(document).ready( function(){
 
-	fabric.Image.fromURL('img/theMoon.png', function (moonImg) {
-		theMoon = new fabric.Image(moonImg.getElement());
-		theMoon.hasBorders = false;
-		theMoon.hasControls = false;
-		theMoon.scale(0.06);			
-		canvas.add(theMoon);
-		groundLine = canvas.height - (theMoon.height * theMoon.scaleY) - 20;
-		theMoon.center();
-	});
+    const startFall = () => {
+        const moonY = theMoon.top + (theMoon.scaleY * theMoon.height);
 
-
-    const startFall = (moon) => {
-
-		const moonY = moon.top + (moon.scaleY * moon.height);
-
-		 // если луна над землей
+        // если луна над землей
         if ( moonY < groundLine ) {
-			// отключаем кнопку во время анимации
-	        $( "#startBtn" ).addClass("disabled");
-	        $( "#startBtn" ).off("click");
 
-			moon.animate('top', 400, {
-				duration: 2000,
-				onChange: canvas.renderAll.bind(canvas),
-				easing: fabric.util.ease.easeOutBounce,
-				onComplete: function() {
-					// включаем кнопку обратно и выводим подсказку
-					$( "#startBtn" ).removeClass("disabled");
-					$( "#startBtn" ).click(() => startFall(theMoon));
-					$( "#tip p" ).text("Get the Moon UP and try again!");
-				}
-			});
+            // отключаем кнопку во время анимации
+            $( "#startBtn" ).addClass("disabled");
+            $( "#startBtn" ).off("click");
+
+            theMoon.animate('top', groundLine, {
+                duration: 2000,
+                onChange: canvas.renderAll.bind(canvas),
+                easing: fabric.util.ease.easeOutBounce,
+                onComplete: function() {
+                    // включаем кнопку обратно и выводим подсказку
+                    $( "#startBtn" ).removeClass("disabled");
+                    $( "#startBtn" ).click(() => startFall());
+                    $( "#tip p" ).text("Get the Moon UP and try again!");
+                }
+            });
         }
+    };
 
-	};
+    const canvas = buildCanvas();
+    let groundLine;
 
-	$( "#startBtn" ).click(() => startFall(theMoon));
+    fabric.Image.fromURL('img/theMoon.png', function (moonImg) {
+        theMoon = new fabric.Image(moonImg.getElement());
+        theMoon.hasBorders = false;
+        theMoon.hasControls = false;
+        theMoon.scale(0.06);
+        canvas.add(theMoon);
+        groundLine = canvas.height - (theMoon.height * theMoon.scaleY) - 20;
+        theMoon.center();
+    });
+
+    $( "#startBtn" ).click(() => startFall());
 
 })
